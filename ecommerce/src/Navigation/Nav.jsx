8103bc8000e6b512330a2d2b99357799606dbc98
Pratyush-Chouksey <pratyushchouksey1983@gmail.com>
2025-04-09 22:50:57 +0530

    Fix case sensitivity in Nav.css import

--- ecommerce/src/Navigation/Nav.jsx
+++ ecommerce/src/Navigation/Nav.jsx
@@ -1,6 +1,6 @@
 import { FiHeart } from "react-icons/fi";
 import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
-import "./Nav.css";
+import "./nav.css";
 
 const Nav = ({ handleInputChange, query }) => {
   return (
